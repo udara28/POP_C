4bffef72c65896de3038bcaa6ace7091ec2b71f8
udara28 <udara28@live.com>
2014-12-07 20:30:39 +0530

    Added remember me feature

---
+++ popc-web/src/Login/LoginBundle/Modals/Login.php
@@ -0,0 +1,35 @@
+<?php
+namespace Login\LoginBundle\Modals;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Login
+ *
+ * @author Udara
+ */
+class Login {
+    private $username;
+    private $password;
+    
+    function getUsername() {
+        return $this->username;
+    }
+
+    function getPassword() {
+        return $this->password;
+    }
+
+    function setUsername($username) {
+        $this->username = $username;
+    }
+
+    function setPassword($password) {
+        $this->password = $password;
+    }
+
+
+}
