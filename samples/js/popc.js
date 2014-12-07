352364cb9a0e9d78756ba0a663f8d92866359da4
Dushan <usefoss4ever@gmail.com>
2014-12-07 12:10:26 +0530

    Added Js example to get community list

--- samples/js/popc.js
+++ samples/js/popc.js
@@ -5,10 +5,27 @@ function translate(params, url) {
         dataType: "json",
         data: jQuery.parseJSON(params),
         success: function (data) {
-            console.log(data['result']);
-            img = "data:image/jpg;base64,"+data['result'];
-            imgTag = "<img src="+img+">";
+            img = "data:image/jpg;base64," + data['result'];
+            imgTag = "<img src=" + img + ">";
             $("#imageDiv").html(imgTag);
+        }
+    });
+}
+
+function getCommunities(params, url) {
+    $.ajax({
+        type: "POST",
+        url: "http://localhost/pop_c/popc-api/index.php?do=" + url,
+        dataType: "json",
+        data: jQuery.parseJSON(params),
+        success: function (data) {
+            console.log(data['result']);
+            var dropDown = $("#comm");
+            for (var x in data['result']) {
+                var option = "<option value='"+data['result'][x]['id']+"'>"+data['result'][x]['name']+"</option>";
+                dropDown.append(option);
+                console.log("X="+ data['result'][x]['name']);
+            }
         }
     });
 }
